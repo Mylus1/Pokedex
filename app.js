8d997eca751c025ea6013608a403935452eb1d28
Bilal Leadbitter <bilal.leadbitter@gmail.com>
2021-05-19 12:32:23 +0100

    mapped data and added Promises for use later

--- app.js
+++ app.js
@@ -2,21 +2,24 @@ console.log("Running Pokedex...")
 
 //Retrieve the data via the api and return it to the console
 const retrievePokemon = () => {
-    const url = 'https://pokeapi.co/api/v2/pokemon/1';
-    fetch(url, {method: 'GET'})
-        .then(res => res.json())
-        
-        .then( data => {
-            console.log(data);
-            const pokemon = {
-                name: data.name,
-                id: data.id,
-                image: data.sprites['front_default'],
-                type: data.types
-                    .map((type) => type.type.name)
-                    .join(', ')}
-                console.log(pokemon)
-            });
-};
+    const promises = [];
+    for (let i = 1; i<= 150; i++)
+        var url = 'https://pokeapi.co/api/v2/pokemon/1';
+        promises.push(fetch(url, {method: 'GET'}).then(res => res.json()))
 
-retrievePokemon()
+//Converting information gathered into useable data
+    Promise.all(promises).then( results => {
+        const pokemon = results.map( (data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types
+                .map((type) => type.type.name)
+                .join(', ')}
+        ))
+    })
+};
+//Logging information into the console for use later
+const displayPokemon = (pokemon) => {
+    console.log(pokemon)
+}
